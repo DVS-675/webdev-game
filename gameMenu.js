6a8711510874e2d445d1205522d2dca78c9815e5
DVS-675 <dmitryvs20170@gmail.com>
2023-05-15 12:35:18 +0500

    переделал все

--- gameMenu.js
+++ gameMenu.js
@@ -1,50 +1,38 @@
 import { startGame } from "./startGame.js";
 
 export const createGameMenu = () => {
-  const title = document.createElement("h2");
-  title.textContent = "Выбери сложность";
-  title.classList.add("start__main_text");
+  const title = document.querySelector(".start__main_text");
+  const appEl = document.querySelector(".game__container");
+  appEl.innerHTML = "";
 
-  const buttonStart = document.createElement("button");
-  buttonStart.textContent = "Старт";
-  buttonStart.classList.add("start__main_button");
-
-  const gameSectionStart = document.querySelector(".game__container");
-  gameSectionStart.innerHTML = "";
-
-  const buttonLevelBox = document.createElement("div");
-  buttonLevelBox.classList.add("start__main_level");
-
-  const createDifficultButton = (difficult) => {
-    const buttonLevel = document.createElement("button");
-    buttonLevel.classList.add("start__main_level-button");
-    buttonLevel.textContent = `${difficult}`;
-
-    buttonLevel.addEventListener("click", () => {
-      if (buttonLevel.classList.contains("clicked")) {
-        buttonLevel.classList.remove("clicked");
-      } else {
-        buttonLevel.classList.add("clicked");
-      }
-    });
-
-    return buttonLevel;
-  };
-
-  gameSectionStart.append(
-    title,
-    createDifficultButton(1),
-    createDifficultButton(2),
-    createDifficultButton(3),
-    buttonStart
-  );
-
+  appEl.innerHTML = `
+  <h2 class="start__main_text">Выбери сложность</h2>
+    <div class ='start__main_level'>
+        <label>
+            <input class = 'start__main_level-button' type="radio" name="level" value="1">
+            <span>1</span>
+        </label>
+        <label>
+            <input class = 'start__main_level-button' type="radio" name="level" value="2">
+            <span>2</span>
+        </label>
+        <label>
+            <input class = 'start__main_level-button' type="radio" name="level" value="3">
+            <span>3</span>
+        </label>
+    </div>
+  <button class="start__main_button">Старт</button>
+  `;
+ 
   const chooseDifficultButtons = document.querySelectorAll(
     ".start__main_level-button"
   );
+  const buttonStart = document.querySelector(".start__main_button");
+  console.log(chooseDifficultButtons);
+
   for (let chooseDifficultButton of chooseDifficultButtons) {
     chooseDifficultButton.addEventListener("click", () => {
-      const gameLevel = Number(chooseDifficultButton.textContent);
+      const gameLevel = Number(chooseDifficultButton.value);
       buttonStart.addEventListener("click", () => startGame(gameLevel));
     });
   }
--- script.js
+++ script.js
@@ -6,16 +6,4 @@ const cardsApp = () => {
 
 cardsApp();
 
-/* function chooseDifficult() {
-  const difficultLevelButtons = document.querySelectorAll(
-    ".start__main_level_button"
-  );
-  for (let difficultLevelButton of difficultLevelButtons) {
-    difficultLevelButton.addEventListener("click", () => {
-      window.rate = difficultLevelButton.value;
 
-      alert("Выбран уровень сложности " + window.rate);
-    });
-  }
-}
-chooseDifficult(); */
